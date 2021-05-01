6171ef6161e6fdc3d5665c983a0f0dd76d05f151
Alexey Venderov <alexey.venderov@getmoss.com>
2021-05-01 14:52:22 +0200

    first commit

---
+++ app/src/main/java/com/github/avenderov/App.java
@@ -0,0 +1,12 @@
+package com.github.avenderov;
+
+public class App {
+
+    public static void main(String[] args) {
+        println("Hello World!");
+    }
+
+    private static void println(String str) {
+        System.out.println(str);
+    }
+}
---
+++ app/src/test/java/com/github/avenderov/AppTest.java
@@ -0,0 +1,13 @@
+package com.github.avenderov;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+
+    @Test
+    void shouldPass() {
+        assertTrue(true);
+    }
+}
